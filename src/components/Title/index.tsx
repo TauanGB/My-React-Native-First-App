a1e68d0562594fd2e74ab4483c2d3b1305660614
TauanGB <tauan.zaque@gmail.com>
2026-03-15 18:35:50 -0300

    Aula 3 Parte 1

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Text, View } from "react-native";
+import Styles from "./style"
 
 export default function Title() {
   return (
-    <View>
-      <Text>OneBithHealth</Text>
+    <View style={Styles.boxTitle}>
+      <Text style={Styles.textTitle}>ONEBITHEALTH</Text>
     </View>
   );
 }
