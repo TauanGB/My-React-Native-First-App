714b1d2f4aaab1291577e353079e36c7c02fbdff
TauanGB <tauan.zaque@gmail.com>
2026-04-17 09:56:02 -0300

    Aula 4 Parte 2

--- src/components/Form/ResultImc/index.tsx
+++ src/components/Form/ResultImc/index.tsx
@@ -1,15 +1,40 @@
 import React from "react";
-import { Text, View, TextInput, Button } from "react-native";
+import { Text, View, TextInput, Button, Share, TouchableOpacity } from "react-native";
 import styles from "./style"
 
 export default function ResultImc(props: { resultado: string; mensagemResultImc: string }) {
+
+	const onShare = async () => {
+		const result = await Share.share({
+			message: 'Meu imc hoje é: ' + props.resultado,
+		})
+
+	}
+
 	return (
 		<View style={styles.adviceBox}>
+			<View style={styles.boxShare}>
+				{props.resultado != "" ?
+
+					<TouchableOpacity
+						style={styles.share}
+						onPress={onShare}
+					>
+						<Text
+						style={styles.textShare}
+						>
+							Share
+						</Text>
+					</TouchableOpacity>
+					:
+					<View></View>
+				}
+			</View>
 			<Text
-			style={styles.adviceText}
+				style={styles.adviceText}
 			>{props.mensagemResultImc}</Text>
 			<Text
-			style={styles.infoText}
+				style={styles.infoText}
 			>{props.resultado}</Text>
 		</View>
 	);
--- src/components/Form/ResultImc/style.ts
+++ src/components/Form/ResultImc/style.ts
@@ -8,17 +8,33 @@ const styles = StyleSheet.create({
 	},
 	adviceBox: {
 		flex: 1,
-		marginTop:15,
-		width:'100%',
-		alignItems:'center',
-		justifyContent:'center',
-		borderRadius:50,
+		marginTop: 15,
+		width: '100%',
+		alignItems: 'center',
+		justifyContent: 'center',
+		borderRadius: 50,
 		backgroundColor: '#c3c3c3'
 	},
 	infoText: {
 		fontSize: 48,
 		fontWeight: "bold",
 		color: '#b64a07',
+	},
+	boxShare: {
+		width: '100%',
+		alignItems: 'center',
+		marginBottom: 10,
+	},
+	share: {
+		backgroundColor: 'blue',
+		borderRadius: 50,
+		paddingBottom: 5,
+		paddingTop: 5,
+	},
+	textShare: {
+		color: '#ffffff',
+		fontWeight: 'bold',
+		paddingHorizontal: 30,
 	}
 })
 
